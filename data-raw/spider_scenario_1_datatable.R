ed529784d4085a1a3f9da6c11eb2fc96f3da796f
Neves-P <10762799+Neves-P@users.noreply.github.com>
2020-12-16 17:34:32 +0000

    spider_scenario_1 -> spider_scenario_1_datable

---
+++ data-raw/spider_scenario_1_datatable.R
@@ -0,0 +1,12 @@
+## code to prepare `spider_scenario_1_datatable` dataset goes here
+
+file_path <- system.file("extdata", "spider_scenario_1.csv",
+                         package = "hawaiispiders")
+spider_scenario_1_datatable <- readr::read_delim(
+  file_path,
+  ";",
+  escape_double = FALSE,
+  trim_ws = TRUE
+)
+
+usethis::use_data(spider_scenario_1_datatable, overwrite = TRUE)
