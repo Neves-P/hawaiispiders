5f7197876bde531f340a03578605cb006517d7a1
Neves-P <10762799+Neves-P@users.noreply.github.com>
2020-12-16 00:30:51 +0000

    Add default_params_doc

---
+++ R/default_params_doc.R
@@ -0,0 +1,19 @@
+#' Default parameters documentation
+#'
+#' @param model Integer with model that should run
+#' @param r_lamc Uniformly sampled value of cladogenesis rate
+#' @param r_mu Uniformly sampled value of extinction rate
+#' @param r_K Uniformly sampled value of carrying capacity
+#' @param r_gam Uniformly sampled value of immigration rate
+#' @param r_ana Uniformly sampled value of anagenesis rate
+#'
+#' @return Nothing
+#' @export
+default_params_doc <- function(model,
+                               r_lamc,
+                               r_mu,
+                               r_K,
+                               r_gam,
+                               r_ana) {
+  # Nothing
+}
--- R/setup_model_parameters.R
+++ R/setup_model_parameters.R
@@ -1,11 +1,18 @@
-#' Title
+#' Stub
 #'
-#' @param variables
+#' @inheritParams default_params_doc
 #'
-#' @return
-#' @export
+#' @return Model arguments
 #'
 #' @examples
+#' # setup_model_arguments(
+#' #   model = model,
+#' #   r_lamc = r_lamc,
+#' #   r_mu = r_mu,
+#' #   r_K = r_K,
+#' #   r_gam = r_gam,
+#' #   r_ana = r_ana
+#' # )
 setup_model_arguments <- function(model, r_lamc, r_mu, r_K, r_gam, r_ana) {
 
 }
