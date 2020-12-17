988e89eab450761aaedf8b7c3d478e9b34df29ef
Neves-P <10762799+Neves-P@users.noreply.github.com>
2020-12-17 00:00:16 +0000

    Export and complete run_spider_analysis

--- R/default_params_doc.R
+++ R/default_params_doc.R
@@ -1,11 +1,16 @@
 #' Default parameters documentation
 #'
-#' @param model Integer with model that should run
-#' @param r_lamc Uniformly sampled value of cladogenesis rate
-#' @param r_mu Uniformly sampled value of extinction rate
-#' @param r_K Uniformly sampled value of carrying capacity
-#' @param r_gam Uniformly sampled value of immigration rate
-#' @param r_ana Uniformly sampled value of anagenesis rate
+#' @param model Numeric integer with model that should run
+#' @param r_lamc Numeric. Uniformly sampled value of cladogenesis rate
+#' @param r_mu Numeric. Uniformly sampled value of extinction rate
+#' @param r_K Numeric. Uniformly sampled value of carrying capacity
+#' @param r_gam Numeric. Uniformly sampled value of immigration rate
+#' @param r_ana Numeric. Uniformly sampled value of anagenesis rate
+#' @param datalist_name Character with name of datalist to be used. Should be
+#'   an exported dataset found in `/data` accessible by [data()]
+#' @param M Numeric integer with mainland species number. In this case it refers
+#'   to the total number of species on the Hawaiian archipelago. Defaults to 165
+#' @param seed Integer to be used as seed
 #'
 #' @return Nothing
 #' @export
@@ -14,6 +19,9 @@ default_params_doc <- function(model,
                                r_mu,
                                r_K,
                                r_gam,
-                               r_ana) {
+                               r_ana,
+                               datalist_name,
+                               M,
+                               seed) {
   # Nothing
 }
