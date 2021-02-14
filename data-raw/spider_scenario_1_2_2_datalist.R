90e5ecea968219b7631bd732c1aafe6465bd6213
Neves-P <p.m.santos.neves@rug.nl>
2021-02-14 23:27:07 +0100

    Add 1.2.2. scenario

---
+++ data-raw/spider_scenario_1_2_2_datalist.R
@@ -0,0 +1,11 @@
+## code to prepare `spider_scenario_1_2_2_datalist` dataset goes here
+
+data("spider_scenario_1_2_2_datatable")
+
+spider_scenario_1_2_2_datalist <- DAISIE::DAISIE_dataprep(
+  spider_scenario_1_2_2_datatable,
+  island_age = 2.4,
+  M = 165
+)
+
+usethis::use_data(spider_scenario_1_2_2_datalist, overwrite = TRUE)
