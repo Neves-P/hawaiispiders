c2174e2be2d4a576a9c1e2980cbcb33b3ff31373
Neves-P <p.m.santos.neves@rug.nl>
2021-02-15 20:04:29 +0100

    Proper test-run_spider_analysis

--- tests/testthat/test-run_spider_analysis.R
+++ tests/testthat/test-run_spider_analysis.R
@@ -1,25 +1,19 @@
 test_that("multiplication works", {
-  testthat::local_edition(x = 3)
   datalist_name <- "spider_scenario_1_1_1_datalist"
   M <- 165
   model <- 1
   seed <- 1
 
-  cluster_folder <- system.file("data", paste0(datalist_name, ".rda"),
-                                package = "hawaiispiders")
-  file_path <- file.path(
-    cluster_folder,
-    paste0(datalist_name, "_", "m_", model, ".txt")
-  )
+  file_path <- create_output_folder(output_name = datalist_name, model = model)
 
-  expect_snapshot_value(x =
-    run_spider_analysis(
+  out <- run_spider_analysis(
       datalist_name = datalist_name,
       M = M,
       model = model,
       seed = seed,
       file_path = file_path
-    ),
-    style = "json"
   )
+  # Clean-up
+  expect_equal(unlink("spiders", recursive = TRUE), 0)
+
 })
