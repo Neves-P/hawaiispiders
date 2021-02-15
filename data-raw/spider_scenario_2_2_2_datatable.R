f0feb393bf40a1c20caaaf4f31de11a577b17d14
Neves-P <p.m.santos.neves@rug.nl>
2021-02-15 15:33:43 +0100

    Add 2.2.2 scenario data

---
+++ data-raw/spider_scenario_2_2_2_datatable.R
@@ -0,0 +1,12 @@
+## code to prepare `spider_scenario_2_2_2_datatable` dataset goes here
+
+file_path <- system.file("extdata", "spider_scenario_2_2_2.csv",
+                         package = "hawaiispiders")
+spider_scenario_2_2_2_datatable <- readr::read_delim(
+  file_path,
+  ";",
+  escape_double = FALSE,
+  trim_ws = TRUE
+)
+
+usethis::use_data(spider_scenario_2_2_2_datatable, overwrite = TRUE)
