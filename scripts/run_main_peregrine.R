c8892067854d441a228d7f9dc3edc0c6bc575906
Neves-P <p.m.santos.neves@rug.nl>
2021-02-17 13:05:19 +0100

    Load package on peregrine to use data

--- scripts/run_main_peregrine.R
+++ scripts/run_main_peregrine.R
@@ -2,6 +2,8 @@ args <- commandArgs(TRUE)
 
 seed_range <- c(as.numeric(args[4]), as.numeric(args[5]))
 
+library(hawaiispiders)
+
 hawaiispiders::run_main(
   datalist_name = args[1],
   M = as.numeric(args[2]),
