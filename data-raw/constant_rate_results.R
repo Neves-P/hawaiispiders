f7dcc46fa7ba75a1461144636193ed8295784619
Neves-P <p.m.santos.neves@rug.nl>
2021-04-05 01:59:17 +0200

    Delete file

--- data-raw/constant_rate_results.R
+++
@@ -1,7 +0,0 @@
-## code to prepare `constant_rate_results` dataset goes here
-
-constant_rate_results <- DAISIEutils::load_txt_results(
-  system.file("extdata", "results", package = "hawaiispiders")
-)
-
-usethis::use_data(constant_rate_results, overwrite = TRUE)
