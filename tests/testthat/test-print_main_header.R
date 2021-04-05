99abe5dbce226e46ade5428570070f31c63fff23
Neves-P <p.m.santos.neves@rug.nl>
2021-04-05 18:21:46 +0200

    Delete tests

--- tests/testthat/test-print_main_header.R
+++
@@ -1,8 +0,0 @@
-test_that("Printing to console works", {
-  datalist_name <- "test name"
-  M <- 10
-  model <- 1
-  expect_message(
-    print_main_header(datalist_name = datalist_name, M = M, model = model)
-  )
-})
