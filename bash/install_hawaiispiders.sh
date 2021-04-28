16498113633b1ecac717844ae70d60bd0e6a3b17
Neves-P <p.m.santos.neves@rug.nl>
2021-04-28 14:31:49 +0200

    Add install_hawaiispiders

---
+++ bash/install_hawaiispiders.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --time=1:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=install_hawaiispiders
+#SBATCH --output=install_hawaiispiders.log
+#SBATCH --mem=2GB
+#SBATCH --partition=gelifes
+
+mkdir -p logs
+ml R
+
+Rscript -e 'remotes::install_github("Neves-P/hawaiispiders")'
