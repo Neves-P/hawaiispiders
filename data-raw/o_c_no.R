e634a2f7fa86f61db0808c93dbaa159eea7b2aac
Neves-P <p.m.santos.neves@rug.nl>
2021-11-04 17:57:45 +0100

    Fix o_c_no

--- data-raw/o_c_no.R
+++ data-raw/o_c_no.R
@@ -5,7 +5,7 @@ data("o_c_no_datatable")
 o_c_no <- DAISIE::DAISIE_dataprep(
   o_c_no_datatable,
   island_age = 2.4,
-  M = 165
+  M = 168
 )
 
 usethis::use_data(o_c_no, overwrite = TRUE)
--- data-raw/o_c_no_datatable.R
+++ data-raw/o_c_no_datatable.R
@@ -3,11 +3,6 @@
 file_path <- system.file("extdata", "o_c_no.csv",
   package = "hawaiispiders"
 )
-o_c_no_datatable <- readr::read_delim(
-  file_path,
-  ";",
-  escape_double = FALSE,
-  trim_ws = TRUE
-)
+o_c_no_datatable <- readr::read_csv(file_path, trim_ws = TRUE)
 
 usethis::use_data(o_c_no_datatable, overwrite = TRUE)
