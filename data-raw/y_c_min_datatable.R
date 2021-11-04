a6f1b9a56f21e230b43cfaf6c2664159596e715e
Neves-P <p.m.santos.neves@rug.nl>
2021-11-04 19:13:49 +0100

    Fix y_c_min

--- data-raw/y_c_min_datatable.R
+++ data-raw/y_c_min_datatable.R
@@ -3,11 +3,6 @@
 file_path <- system.file("extdata", "y_c_min.csv",
   package = "hawaiispiders"
 )
-y_c_min_datatable <- readr::read_delim(
-  file_path,
-  ";",
-  escape_double = FALSE,
-  trim_ws = TRUE
-)
+y_c_min_datatable <- readr::read_csv(file_path, trim_ws = TRUE)
 
 usethis::use_data(y_c_min_datatable, overwrite = TRUE)
