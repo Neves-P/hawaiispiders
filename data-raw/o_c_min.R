8f1d64a0fae86882e0592c239def1a2090bb5384
Neves-P <p.m.santos.neves@rug.nl>
2021-11-04 17:57:08 +0100

    Fix o_c_min

--- data-raw/o_c_min.R
+++ data-raw/o_c_min.R
@@ -5,7 +5,7 @@ data("o_c_min_datable")
 o_c_min <- DAISIE::DAISIE_dataprep(
   o_c_min_datable,
   island_age = 2.4,
-  M = 165
+  M = 168
 )
 
 usethis::use_data(o_c_min, overwrite = TRUE)
--- data-raw/o_c_min_datable.R
+++ data-raw/o_c_min_datable.R
@@ -3,11 +3,6 @@
 file_path <- system.file("extdata", "o_c_min.csv",
   package = "hawaiispiders"
 )
-o_c_min_datable <- readr::read_delim(
-  file_path,
-  ";",
-  escape_double = FALSE,
-  trim_ws = TRUE
-)
+o_c_min_datable <- readr::read_csv(file_path, trim_ws = TRUE)
 
 usethis::use_data(o_c_min_datable, overwrite = TRUE)
