6912e024a63a36edb03dbf9d0068cc2f713a464f
Neves-P <p.m.santos.neves@rug.nl>
2021-11-14 20:06:06 +0100

    Fix y_m_no

--- data-raw/y_m_no.R
+++ data-raw/y_m_no.R
@@ -5,7 +5,7 @@ data("y_m_no_datatable")
 y_m_no <- DAISIE::DAISIE_dataprep(
   y_m_no_datatable,
   island_age = 1.2,
-  M = 165
+  M = 168
 )
 
 usethis::use_data(y_m_no, overwrite = TRUE)
--- data-raw/y_m_no_datatable.R
+++ data-raw/y_m_no_datatable.R
@@ -3,11 +3,7 @@
 file_path <- system.file("extdata", "y_m_no.csv",
   package = "hawaiispiders"
 )
-y_m_no_datatable <- readr::read_delim(
-  file_path,
-  ";",
-  escape_double = FALSE,
-  trim_ws = TRUE
-)
+
+y_m_no_datatable <- readr::read_csv(file_path)
 
 usethis::use_data(y_m_no_datatable, overwrite = TRUE)
