ffb532c663f0e468da1ec618817fb1ce469e011a
Neves-P <p.m.santos.neves@rug.nl>
2021-11-14 20:48:04 +0100

    Fix y_m_max

--- data-raw/y_m_max.R
+++ data-raw/y_m_max.R
@@ -5,7 +5,7 @@ data("y_m_max_datatable")
 y_m_max <- DAISIE::DAISIE_dataprep(
   y_m_max_datatable,
   island_age = 1.2,
-  M = 165
+  M = 168
 )
 
 usethis::use_data(y_m_max, overwrite = TRUE)
--- data-raw/y_m_max_datatable.R
+++ data-raw/y_m_max_datatable.R
@@ -3,11 +3,6 @@
 file_path <- system.file("extdata", "y_m_max.csv",
   package = "hawaiispiders"
 )
-y_m_max_datatable <- readr::read_delim(
-  file_path,
-  ";",
-  escape_double = FALSE,
-  trim_ws = TRUE
-)
+y_m_max_datatable <- readr::read_csv(file_path)
 
 usethis::use_data(y_m_max_datatable, overwrite = TRUE)
