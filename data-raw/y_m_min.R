409a9ebd9360a50f91b08640ea51c264c6ded736
Neves-P <p.m.santos.neves@rug.nl>
2021-11-14 20:10:24 +0100

    Fix y_m_min

--- data-raw/y_m_min.R
+++ data-raw/y_m_min.R
@@ -5,7 +5,7 @@ data("y_m_min_datatable")
 y_m_min <- DAISIE::DAISIE_dataprep(
   y_m_min_datatable,
   island_age = 1.2,
-  M = 165
+  M = 168
 )
 
 usethis::use_data(y_m_min, overwrite = TRUE)
--- data-raw/y_m_min_datatable.R
+++ data-raw/y_m_min_datatable.R
@@ -3,11 +3,6 @@
 file_path <- system.file("extdata", "y_m_min.csv",
   package = "hawaiispiders"
 )
-y_m_min_datatable <- readr::read_delim(
-  file_path,
-  ";",
-  escape_double = FALSE,
-  trim_ws = TRUE
-)
+y_m_min_datatable <- readr::read_csv(file_path)
 
 usethis::use_data(y_m_min_datatable, overwrite = TRUE)
