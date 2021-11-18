f07a84c64ba115594910f1ebc91212ff16153b7c
Neves-P <p.m.santos.neves@rug.nl>
2021-11-18 20:35:13 +0100

    Fix y_c_no

--- data-raw/y_c_no_datatable.R
+++ data-raw/y_c_no_datatable.R
@@ -3,11 +3,6 @@
 file_path <- system.file("extdata", "y_c_no.csv",
   package = "hawaiispiders"
 )
-y_c_no_datatable <- readr::read_delim(
-  file_path,
-  ";",
-  escape_double = FALSE,
-  trim_ws = TRUE
-)
+y_c_no_datatable <- readr::read_csv(file_path)
 
 usethis::use_data(y_c_no_datatable, overwrite = TRUE)
