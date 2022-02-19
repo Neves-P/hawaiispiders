4f3592d2b82bba2c7b19e1362fb9a42ecdec48a6
Neves-P <p.m.santos.neves@rug.nl>
2022-02-19 18:58:14 +0100

    EOD

---
+++ cross_optimmethod_test.R
@@ -0,0 +1,51 @@
+full_res <- readRDS("full_res.rds")
+prepared_out <- prepare_results_to_plot(full_res)
+
+scenarios <- unique(prepared_out$scenario)
+
+integrator_pairs <- list()
+for (i in seq_along(scenarios)) {
+  integrator_pairs[[i]] <- subset(prepared_out, scenario == scenarios[i])
+}
+
+out_list <- vector(mode = "list", length = length(scenarios))
+names(out_list) <- scenarios
+model_params <- DAISIEutils::setup_model("cr_dd")
+
+
+for (i in seq_along(scenarios)) {
+  initparsopt_odeint <- integrator_pairs[[i]][1, 2:6]
+  testit::assert(integrator_pairs[[i]][1, 11] == factor("odeint"))
+
+  initparsopt_lsodes <- integrator_pairs[[i]][2, 2:6]
+  testit::assert(integrator_pairs[[i]][2, 11] == factor("lsodes"))
+
+  res_odeint_lsodes <- DAISIE::DAISIE_ML(
+    datalist = get(scenarios[i]),
+    initparsopt = unlist(initparsopt_odeint),
+    idparsnoshift = model_params$idparsnoshift,
+    idparsopt = model_params$idparsopt,
+    parsfix = model_params$parsfix,
+    idparsfix = model_params$idparsfix,
+    ddmodel = model_params$ddmodel,
+    cond = 5,
+    optimmethod = "lsodes",
+    CS_version = model_params$cs_version
+  )
+  res_lsodes_odeint <- DAISIE::DAISIE_ML(
+    datalist = get(scenarios[i]),
+    initparsopt = unlist(initparsopt_lsodes),
+    idparsnoshift = model_params$idparsnoshift,
+    idparsopt = model_params$idparsopt,
+    parsfix = model_params$parsfix,
+    idparsfix = model_params$idparsfix,
+    ddmodel = model_params$ddmodel,
+    cond = 5,
+    optimmethod = "odeint::runge_kutta_fehlberg78",
+    CS_version = model_params$cs_version
+  )
+
+  testit::assert(names(out_list[i]) == scenarios[i])
+  res <- rbind(res_odeint_lsodes, res_lsodes_odeint)
+  out_list[[i]] <- res
+}
