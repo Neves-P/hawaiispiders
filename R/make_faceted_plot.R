84c4c8dd537d060e2e0d75e00e45d830a4f8e4af
Neves-P <p.m.santos.neves@rug.nl>
2022-02-26 12:07:21 +0100

    Facet using patchwork

--- R/make_faceted_plot.R
+++ R/make_faceted_plot.R
@@ -27,20 +27,23 @@ make_faceted_plot <- function(plotting_table,
       parameter_name = parameter_names[i],
       colour_by = colour_by,
       shape_by = shape_by
-    ) + ggplot2::theme(legend.position = "none")
+    )
   }
-  legend <- cowplot::get_legend(plot_with_legend)
-
-  no_legend_grid <- cowplot::plot_grid(
-    plotlist = estimate_plots,
+  # Default legend settings
+  legend_direction <- "vertical"
+  legend_position <- "right"
+  # If number of plots is odd, add legend on free space.
+  if (!identical(length(estimate_plots) %% 2, 0)) {
+    estimate_plots[[length(estimate_plots) + 1]] <- patchwork::guide_area()
+    legend_direction <- "vertical"
+    legend_position <- "bottom"
+  }
+  grid_with_legend <- patchwork::wrap_plots(
+    estimate_plots,
     ncol = 2,
-    align = "h"
-  )
-
-  grid_with_legend <- cowplot::plot_grid(
-    no_legend_grid,
-    legend,
-    rel_widths = c(3.5, 1)
-  )
+    guides = "collect") & ggplot2::theme(
+      legend.direction = legend_direction,
+      legend.position = legend_position
+    )
   grid_with_legend
 }
