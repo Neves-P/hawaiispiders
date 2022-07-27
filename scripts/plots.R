949cc815a43a618aeaaeb5b26a2e020ee96d2045
Neves-P <p.m.santos.neves@rug.nl>
2022-07-27 03:30:33 +0200

    EOD

--- scripts/plots.R
+++ scripts/plots.R
@@ -32,15 +32,14 @@ datatables$first_brt[which(is.na(datatables$first_brt) &
                              datatables$age == factor("r", levels = c("a", "o", "y", "r")))] <-
   1.2
 datatables <- dplyr::group_by(datatables, Clade_name)
-datatables <-
-  dplyr::arrange(datatables, dplyr::desc(first_brt), .by_group = TRUE)
+datatables <- dplyr::arrange(datatables, dplyr::desc(first_brt), .by_group = TRUE)
 datatables <- cbind(datatables, id = 1:nrow(datatables))
 
 library(ggplot2)
 
 # Plot
 # Colour lines
-data_plot <- ggplot(datatables, aes(x = id, y = first_brt)) +
+data_plot_1 <- ggplot(datatables, aes(x = id, y = first_brt)) +
   geom_segment(aes(
     x = id,
     xend = id,
@@ -50,8 +49,20 @@ data_plot <- ggplot(datatables, aes(x = id, y = first_brt)) +
   geom_point(size = 3, aes(colour = Status)) +
   theme(axis.text.x = element_blank(),
         axis.title.x = element_blank(), legend.position = "none") +
-  ylab("Time (myr)") +
+  ylab("Time (myr)")
 
+
+data_plot_2 <- ggplot(datatables, aes(y = id, x = first_brt)) +
+  geom_segment(aes(
+    y = id,
+    yend = id,
+    x = 0,
+    xend = first_brt
+  )) +
+  geom_point(size = 3, aes(colour = Status)) +
+  theme(axis.text.x = element_blank(),
+        axis.title.x = element_blank(), legend.position = "none") +
+  ylab("Time (myr)")
 
 # estimates
 
@@ -75,34 +86,59 @@ r_plot <- ggplot2::ggplot(data = r_pivot_to_plot) +
                       ggplot2::aes(x = name,
                                    y = value,
                                    shape = stac)) +
-  ggplot2::xlab("Parameters")
+  ggplot2::xlab("Parameters") +
+  ggplot2::theme(legend.position = "none",
+                 axis.title.y = ggplot2::element_blank()) +
+  ggplot2::scale_x_discrete(
+    labels = c("lambda_c" = "\u03BB\u1D9C",
+               "lambda_a" = "\u03BB\U1D43",
+               "gamma" = "\u03B3",
+               "mu" = "\u03BC",
+               "K" = "K")
+  )
 y_plot <- ggplot2::ggplot(data = y_pivot_to_plot) +
   ggplot2::geom_point(position = ggplot2::position_dodge(0.5),
                       ggplot2::aes(x = name,
                                    y = value,
                                    shape = stac)) +
   ggplot2::theme(axis.title.x = ggplot2::element_blank(),
-                 axis.text.x = ggplot2::element_blank())
+                 axis.text.x = ggplot2::element_blank(),
+                 legend.position = "none",
+                 axis.title.y = ggplot2::element_blank())
 o_plot <- ggplot2::ggplot(data = o_pivot_to_plot) +
   ggplot2::geom_point(position = ggplot2::position_dodge(0.5),
                       ggplot2::aes(x = name,
                                    y = value,
                                    shape = stac)) +
   ggplot2::theme(axis.title.x = ggplot2::element_blank(),
-                 axis.text.x = ggplot2::element_blank())
+                 axis.text.x = ggplot2::element_blank(),
+                 legend.position = "none",
+                 axis.title.y = ggplot2::element_blank())
 a_plot <- ggplot2::ggplot(data = a_pivot_to_plot) +
   ggplot2::geom_point(position = ggplot2::position_dodge(0.5),
                       ggplot2::aes(x = name,
                                    y = value,
                                    shape = stac)) +
   ggplot2::theme(axis.title.x = ggplot2::element_blank(),
-                 axis.text.x = ggplot2::element_blank())
+                 axis.text.x = ggplot2::element_blank(),
+                 legend.position = "none",
+                 axis.title.y = ggplot2::element_blank())
 library(patchwork)
 
-facet_plot <- (data_plot | (a_plot / o_plot / y_plot / r_plot)) + plot_layout(guides = 'collect') + theme(legend.position = "none")
+facet_plot_1 <- (data_plot_1 | (a_plot / o_plot / y_plot / r_plot)) + plot_layout(widths = c(2, 1))
+facet_plot_2 <- (data_plot_2 / (a_plot | o_plot | y_plot | r_plot)) + plot_layout(heights = c(2, 1))
 ggplot2::ggsave(
-  plot = facet_plot,
-  filename = paste0("facet_plot.png"),
+  plot = facet_plot_1,
+  filename = paste0("facet_plot_1.png"),
+  device = "png",
+  width = 168,
+  height = 200,
+  units = "mm"
+)
+
+ggplot2::ggsave(
+  plot = facet_plot_2,
+  filename = paste0("facet_plot_2.png"),
   device = "png",
   width = 168,
   height = 200,
