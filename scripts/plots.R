b8413c1778f26429ceec545144bcb2ab223767bc
Neves-P <p.m.santos.neves@rug.nl>
2022-09-02 12:16:39 +0200

    Figs

--- scripts/plots.R
+++ scripts/plots.R
@@ -22,16 +22,53 @@ datatables$first_brt[
   which(is.na(datatables$first_brt) &
           datatables$age == factor("r", levels = c("a", "o", "y", "r"))
   )] <- 1.2
+
+
+datatables$first_brt[
+  which(datatables$first_brt > 4.8 &
+          datatables$age == factor("a", levels = c("a", "o", "y", "r")))] <- 4.8
+
+datatables$first_brt[
+  which(datatables$first_brt > 3.6 &
+          datatables$age == factor("o", levels = c("a", "o", "y", "r")))] <- 3.6
+
+datatables$first_brt[
+  which(datatables$first_brt > 2.4 &
+          datatables$age == factor("y", levels = c("a", "o", "y", "r")))] <- 2.4
+datatables$first_brt[
+  which(datatables$first_brt > 1.2 &
+          datatables$age == factor("r", levels = c("a", "o", "y", "r")))] <- 1.2
+
 datatables <- dplyr::group_by(datatables, Clade_name)
 datatables <- dplyr::arrange(datatables, dplyr::desc(first_brt), .by_group = TRUE)
-datatables <- cbind(datatables, id = 1:nrow(datatables))
 
 library(ggplot2)
 
-data_plot <- ggplot(datatables, aes(y = id, x = first_brt)) +
+datatables$Status <- forcats::fct_recode(
+  datatables$Status,
+  EndemicPrecise = "Endemic",
+  NonEndemicPrecise = "Non_Endemic",
+  EndemicUpperBound = "Endemic_MaxAge",
+  EndemicUpperLowerBound = "Endemic_MaxAgeMinAge",
+  NonEndemicUpperBound = "Non_Endemic_MaxAge",
+  NonEndemicUpperLowerBound = "Non_Endemic_MaxAgeMinAge"
+)
+datatables$Status <- forcats::fct_relevel(
+  datatables$Status,
+  c(
+    "EndemicPrecise",
+    "EndemicUpperBound",
+    "NonEndemicPrecise",
+    "NonEndemicUpperBound",
+    "EndemicUpperLowerBound",
+    "NonEndemicUpperLowerBound"
+  )
+)
+
+data_plot <- ggplot(datatables, aes(y = Clade_name, x = first_brt)) +
   geom_segment(aes(
-    y = id,
-    yend = id,
+    y = Clade_name,
+    yend = Clade_name,
     x = first_brt,
     xend = 0
   )) +
@@ -40,8 +77,20 @@ data_plot <- ggplot(datatables, aes(y = id, x = first_brt)) +
   theme(
     axis.text.y = element_blank(),
     axis.title.y = element_blank(),
-    legend.position = "none"
+    axis.ticks.y = element_blank(),
+    legend.position = "bottom",
+    legend.title = element_blank()
   ) +
+  scale_color_brewer(
+    palette = "Dark2",
+    labels = c(
+      "Endemic precise",
+      "Endemic upper bound",
+      "Non-endemic precise",
+      "Non-endemic upper bound",
+      "Endemic upper and lower bound",
+      "Non-endemic upper and lower bound"
+    )) +
   xlab("Time (myr)") +
   geom_vline(xintercept = 1.2, linetype = "dashed", size = 0.4) +
   geom_vline(xintercept = 2.4, linetype = "dashed", size = 0.4) +
@@ -52,13 +101,8 @@ data_plot <- ggplot(datatables, aes(y = id, x = first_brt)) +
 # estimatesdata(res)
 res <- dplyr::filter(res, version == "4.2.1")
 
-save_paper_plot(plot_to_save = data_plot, file_name = "data_plot", type_size = "full_size")
-ggplot2::ggsave(
-  plot = data_plot,
-  filename = file.path("figures", "data_plot.png"),
-  device = "png",
-  width = 168,
-  height = 200,
-  units = "mm"
+save_paper_plot(
+  plot_to_save = data_plot,
+  file_name = "data_plot",
+  type_size = "full_size"
 )
-
