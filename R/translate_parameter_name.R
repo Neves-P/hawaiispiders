c1c3b9191533e0aa722f1c786f0add504bd34e61
Pedro Santos Neves <10762799+Neves-P@users.noreply.github.com>
2022-11-18 17:28:20 +0100

    English parameter names

--- R/translate_parameter_name.R
+++ R/translate_parameter_name.R
@@ -10,11 +10,11 @@
 translate_parameter_name <- function(parameter_name) {
   out <- switch(
     parameter_name,
-    "lambda_c" = "\u03BB\u1D9C",
-    "lambda_a" = "\u03BB\U1D43",
-    "gamma" = "\u03B3",
-    "mu" = "\u03BC",
-    "K" = "K"
+    "lambda_c" = "Cladogenesis rate",
+    "lambda_a" = "Cladogenesis rate",
+    "gamma" = "Colonisation rate",
+    "mu" = "Extinction rate",
+    "K" = "Carrying capacity"
   )
 
   out
