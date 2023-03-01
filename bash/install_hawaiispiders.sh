d404d96592614672ba1f9471d7b5235008e7f979
Neves-P <p.m.santos.neves@rug.nl>
2023-03-01 18:20:06 +0100

    Force install

--- bash/install_hawaiispiders.sh
+++ bash/install_hawaiispiders.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --time=00:30:00
+#SBATCH --time=01:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=install_hawaiispiders
 #SBATCH --output=install_hawaiispiders.log
 #SBATCH --mem=2GB
-#SBATCH --partition=short
+#SBATCH --partition=regular
 
 mkdir -p logs
 ml R
 
-Rscript -e 'remotes::install_github("rsetienne/DAISIE", upgrade = TRUE)'
-Rscript -e 'remotes::install_github("tece-lab/DAISIEutils", upgrade = TRUE)'
-Rscript -e 'remotes::install_github("Neves-P/hawaiispiders", upgrade = TRUE)'
+Rscript -e 'remotes::install_github("rsetienne/DAISIE", upgrade = TRUE, force = TRUE)'
+Rscript -e 'remotes::install_github("tece-lab/DAISIEutils", upgrade = TRUE, force = TRUE)'
+Rscript -e 'remotes::install_github("Neves-P/hawaiispiders", upgrade = TRUE, force = TRUE)'
