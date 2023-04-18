438f42e8429e9c23c57f6b8e588f1eb82850c273
Neves-P <p.m.santos.neves@rug.nl>
2023-04-19 01:11:58 +0200

    TODO: break clades into species

--- data-raw/species_genus_table.R
+++ data-raw/species_genus_table.R
@@ -2,17 +2,15 @@ data("m_master")
 
 # Split by species (upper case)
 
-species_names <-gsub('([[:upper:]])', ' \\1', m_master$Clade_name)
-genus_names <- sapply(strsplit(species_names, "_"), `[[`, 1)
+species_names <- paste0("\textit{", gsub('_', ' \\1', m_master$Clade_name), "}")
 brts_avail <- ifelse(!is.na(m_master$Branching_times), "Available", NA)
 minage_avail <- ifelse(!is.na(m_master$MinAge), "Available", NA)
 
 species_genus_table <- data.frame(
-  genus_names = genus_names,
   species_names = species_names,
   brts_avail = brts_avail,
   minage_avail = minage_avail
 )
 
 write.csv(species_genus_table, "species_genus_table.csv")
-
+xtable::xtable(species_genus_table)
