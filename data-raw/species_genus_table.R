9b547e00497b8ef7dcc55039eebcc69ed3923451
Neves-P <p.m.santos.neves@rug.nl>
2024-06-21 18:23:04 +0200

    Add status info that was already on table

--- data-raw/species_genus_table.R
+++ data-raw/species_genus_table.R
@@ -5,6 +5,7 @@ data("m_master")
 species_names <- paste0("\textit{", gsub('_', ' \\1', m_master$Clade_name), "}")
 brts_avail <- ifelse(!is.na(m_master$Branching_times), "Available", NA)
 minage_avail <- ifelse(!is.na(m_master$MinAge), "Available", NA)
+status <- ifelse(m_master$Status == "Endemic", "Endemic", NA)
 
 species_genus_table <- data.frame(
   species_names = species_names,
